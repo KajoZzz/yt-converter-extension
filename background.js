f54f8d2502444ab019e6fad46ab0cde90f354e6b
Kajetan Z <81050236+KajoZzz@users.noreply.github.com>
2022-01-19 19:04:40 +0100

    Update background.js

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-window.videoID = "";
+window.url = "";
 
 chrome.runtime.onStartup.addListener(function() {
     chrome.browserAction.setIcon({ path: "icon.png" });
@@ -9,19 +9,14 @@ chrome.browserAction.onClicked.addListener(function (tab) {
         let url = tabs[0].url;
         if(url.includes("https://www.youtube.com/watch?v="))
         {
-            let id = url.substring(url.lastIndexOf('?v=') + 3);
-            if(url.includes("&t=")){
-                id = id.slice(0, -(id.length - id.lastIndexOf('&t='))); 
-            }
-
-            window.videoID = id;
+            window.url = url;
 
             chrome.windows.create({
                 url: `popup.html`,
                 focused: true,
                 type: "popup",
                 width: 500,
-                height: 260
+                height: 300
             });
         }
     });
