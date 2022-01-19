6ce54ff96770ae3c872b0eb74d54a98b5ecd4c3a
Kajetan Z <81050236+KajoZzz@users.noreply.github.com>
2022-01-19 20:39:37 +0100

    Big update

--- background.js
+++ background.js
@@ -4,20 +4,35 @@ chrome.runtime.onStartup.addListener(function() {
     chrome.browserAction.setIcon({ path: "icon.png" });
 })
 
-chrome.browserAction.onClicked.addListener(function (tab) {
-    chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-        let url = tabs[0].url;
-        if(url.includes("https://www.youtube.com/watch?v="))
-        {
-            window.url = url;
+chrome.contextMenus.create({
+    type: "radio",
+    title: "Bright theme",
+    checked: (window.localStorage.getItem("theme") === "bright"),
+    contexts: ["browser_action"],
+    onclick: function() {
+        window.localStorage.setItem("theme", "bright");
+    }
+});
+chrome.contextMenus.create({
+    type: "radio",
+    title: "Dark theme",
+    checked: (window.localStorage.getItem("theme") === "dark"),
+    contexts: ["browser_action"],
+    onclick: function() {
+        window.localStorage.setItem("theme", "dark");
+    }
+});
 
-            chrome.windows.create({
-                url: `popup.html`,
-                focused: true,
-                type: "popup",
-                width: 500,
-                height: 300
-            });
-        }
-    });
-})
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
+    if(message.popupOpen) { 
+        chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+            if(tabs[0].url === undefined) return
+            
+            let url = tabs[0].url;
+            if(url.includes("https://www.youtube.com/watch?v="))
+            {
+                window.url = url;
+            }
+        });
+    }
+  });
