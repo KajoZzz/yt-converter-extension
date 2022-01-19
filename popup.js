dd589dde52107697fa43a699cc5629ec16703c2f
Kajetan Z <81050236+KajoZzz@users.noreply.github.com>
2022-01-19 20:40:31 +0100

    Big update

--- popup.js
+++ popup.js
@@ -1,19 +1,26 @@
 let bg;
 
 document.addEventListener('DOMContentLoaded', function () {
+    chrome.runtime.sendMessage({popupOpen: true});
     bg = chrome.extension.getBackgroundPage()
+
+    if(bg.localStorage.getItem("theme") === "bright"){
+        if(document.body.classList.contains("dark")) document.body.classList.remove("dark")
+    }
+    else {
+        if(!document.body.classList.contains("dark")) document.body.classList.add("dark")
+    }
 }, false)
 
 const convertBtn = document.querySelector('.convert-button');
-const URLinput = document.querySelector('.URL-input');
 const TYPEinput = document.querySelector('.TYPE-input');
 
 
 convertBtn.addEventListener('click', () => {
-    const url = bg.url
+    const url = bg.url;
     sendURL(url, TYPEinput.value);
 });
 
 function sendURL(URL, TYPE) {
-    window.location.href = `https://youtube-conv.herokuapp.com/download?URL=${URL}&TYPE=${TYPE}`;
+    window.open(`https://youtube-conv.herokuapp.com/download?URL=${URL}&TYPE=${TYPE}`, '_blank');
 }
